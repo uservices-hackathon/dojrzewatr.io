57f66c2c24ff20f4e6d9e8a9c29de7185ee358c5
Damian Pacierpnik <damian.pacierpnik@hybris.com>
2015-11-05 14:54:32 +0100

    to String

--- src/main/java/pl/uservices/dojrzewatr/endpoints/wort/WortDto.java
+++ src/main/java/pl/uservices/dojrzewatr/endpoints/wort/WortDto.java
@@ -1,5 +1,9 @@
 package pl.uservices.dojrzewatr.endpoints.wort;
 
+import static org.apache.commons.lang.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -16,5 +20,13 @@ public class WortDto
 	public Integer getQuantity()
 	{
 		return quantity;
+	}
+
+	@Override
+	public String toString()
+	{
+		return new ToStringBuilder(this, SHORT_PREFIX_STYLE) //
+				.append("quantity", quantity) //
+				.toString();
 	}
 }
