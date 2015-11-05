1565278acd1f4bc456652d17a5c20051a4fe1cf8
Damian Pacierpnik <damian.pacierpnik@hybris.com>
2015-11-05 11:06:26 +0100

    empty clients

---
+++ src/main/java/pl/uservices/dojrzewatr/clients/ButelkatrClient.java
@@ -0,0 +1,19 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ */
+package pl.uservices.dojrzewatr.clients;
+
+public class ButelkatrClient
+{
+	public void sendBeerQuantity(final Integer beerQuantity) {
+		// TODO
+	}
+}
