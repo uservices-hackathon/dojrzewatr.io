7b3024c2245c59c00c39a0a7210a2cb726aead22
Pawel Oczadly <paweloczadly@gmail.com>
2015-11-05 11:06:44 +0100

    contract fixed

--- repository/mappings/pl/uservices/dojrzewatr/brewing/dojrzewatr.groovy
+++ repository/mappings/pl/uservices/dojrzewatr/brewing/dojrzewatr.groovy
@@ -1,17 +1,16 @@
-+io.codearte.accurest.dsl.GroovyDsl.make {
-+	request {
-+		method "POST"
-+		url "/wort"
-+		body('''
-+                    {
-+                    "quantity":1
-+                    }
-+                '''
-+		)
-+		headers { header("Content-Type", "application/json") }
-+	}
-+	response {
-+		status 204
-+		}
-+	}
-+}
+io.codearte.accurest.dsl.GroovyDsl.make {
+	request {
+		method "POST"
+		url "/wort"
+		body('''
+            {
+            "quantity":1
+            }
+        '''
+		)
+		headers { header("Content-Type", "application/json") }
+	}
+	response {
+		status 204
+	}
+}
