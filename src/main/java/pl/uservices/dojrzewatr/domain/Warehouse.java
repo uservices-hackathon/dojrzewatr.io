780b41bc63132f8e13bf8e40a0e1cef72f723399
Damian Pacierpnik <damian.pacierpnik@hybris.com>
2015-11-05 15:05:49 +0100

    dojrzewatr fix

--- src/main/java/pl/uservices/dojrzewatr/domain/Warehouse.java
+++ src/main/java/pl/uservices/dojrzewatr/domain/Warehouse.java
@@ -1,62 +1,54 @@
 package pl.uservices.dojrzewatr.domain;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
 import pl.uservices.dojrzewatr.clients.butelkatr.BeerQuantityDto;
 import pl.uservices.dojrzewatr.clients.butelkatr.ButelkatrClient;
 import pl.uservices.dojrzewatr.clients.prezentatr.PrezentatrClient;
 import pl.uservices.dojrzewatr.clients.prezentatr.WarehouseStateDto;
 
-import java.util.Date;
-import java.util.concurrent.ConcurrentLinkedQueue;
+;
 
 @Component
 public class Warehouse
 {
 	private final static Logger LOGGER = LoggerFactory.getLogger(Warehouse.class);
+	public static final int MATURATION_TIME = 30000;
 
 	@Autowired
 	private PrezentatrClient prezentatrClient;
 	@Autowired
 	private ButelkatrClient butelkatrClient;
 
-	private final ConcurrentLinkedQueue<Wort> worts = new ConcurrentLinkedQueue<Wort>();
+	private AtomicInteger warehouseState = new AtomicInteger();
 
-	@Scheduled(fixedRate = 30000)
-	public void checkWort()
-	{
-		LOGGER.info("checking wort");
-		final long currentTimestamp = new Date().getTime();
-		Integer beerAmount = 0;
-		Integer amountInWarehouse = 0;
+	@Async
+	public void addWort(Wort wort) {
 
-		Wort wort = worts.peek();
-		if (wort != null)
-		{
-			while (currentTimestamp - wort.getTimestamp() >= 10000)
-			{
-				wort = worts.poll();
-				beerAmount += wort.getAmount();
-				wort = worts.peek();
-			}
+		updateStateAndNotify(wort.getAmount());
+
+		try {
+
+			Thread.sleep(MATURATION_TIME);
+
+			butelkatrClient.sendBeerQuantity(new BeerQuantityDto(wort.getAmount()));
+
+			updateStateAndNotify(-wort.getAmount());
 		}
-		if (beerAmount > 0)
-		{
-			for (final Wort w : worts)
-			{
-				amountInWarehouse += w.getAmount();
-			}
-			LOGGER.info("sending beer");
-			butelkatrClient.sendBeerQuantity(new BeerQuantityDto(beerAmount));
-			prezentatrClient.sendWarehouseState(new WarehouseStateDto(amountInWarehouse));
+		catch(final Exception e) {
+			LOGGER.error("Error in sleep.", e);
 		}
 	}
 
-	public void addWort(Wort wort)
+	private void updateStateAndNotify(final int stateChange)
 	{
-		worts.add(wort);
+		final int newState = warehouseState.addAndGet(stateChange);
+		prezentatrClient.sendWarehouseState(new WarehouseStateDto(newState));
 	}
 }
